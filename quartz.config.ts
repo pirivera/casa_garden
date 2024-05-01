7c1c8e243db0f7ff6f1ca8977caac2e0e2030d1c
unknown <pedrophoto20@gmail.com>
2024-04-30 23:57:49 -0400

    Quartz sync: Apr 30, 2024, 11:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -32,7 +32,7 @@ const config: QuartzConfig = {
           lightgray: "#d9b99b",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
+          dark: "#97816c",
           secondary: "#ad947c",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
